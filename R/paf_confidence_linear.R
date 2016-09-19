f576cbdd06976c8c8168e3fdab56b353d68c431b
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2016-09-19 17:14:11 -0500

    Allows for theta and X to be multivariate
    Corrected previous upload that didn't change anything

--- R/paf_confidence_linear.R
+++ R/paf_confidence_linear.R
@@ -1,6 +1,6 @@
 #' @title Approximate Confidence Intervals for the Potential Impact Fraction
 #' 
-#' @description Function that calculates confidence intervals to the potential impact fraction
+#' @description Function that calculates approximate confidence intervals to the potential impact fraction
 #' 
 #' @param X         Random sample (can be vector or matrix) which includes exposure and covariates.
 #' 
@@ -35,11 +35,19 @@
 #' thetavar <- 0.1
 #' paf.confidence.linear(X, thetahat, thetavar, function(X, theta){exp(theta*X)})
 #' 
+#' #Example with theta and X multivariate
+#' set.seed(18427)
+#' X1 <- rnorm(2000)
+#' X2 <- rnorm(2000)
+#' X  <- as.matrix(cbind(X1,X2))
+#' thetahat <- c(0.1, 0.03)
+#' thetasd <- matrix(c(0.1, 0, 0, 0.05), byrow = T, nrow = 2)
+#' paf.confidence.linear(X, thetahat, thetasd, function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}) 
 #' 
 #' @export
 
 
-paf.confidence.linear <- function(X, thetahat, thetasd, rr, weights =  rep(1/nrow(as.matrix(X)),nrow(as.matrix(X))), confidence = 95){
+paf.confidence.linear <- function(X, thetahat, thetasd, rr, weights =  rep(1/nrow(as.matrix(X)),nrow(as.matrix(X))), confidence = 95, nsim = 1000){
   
   #Set the vector for the confidence intervals
   .ci <- c("Lower_CI" = NA, "Point_Estimate" = NA, "Upper_CI" = NA, "Estimated_Variance" = NA)
@@ -48,10 +56,10 @@ paf.confidence.linear <- function(X, thetahat, thetasd, rr, weights =  rep(1/nro
   Z <- qnorm(1 - ((100-confidence)/200))
   
   #Get the point estimate and variance
-  .ci["Point_Estimate"]     <- pif(X, thetahat, rr, weights = weights)
-  .ci["Estimated_Variance"] <- paf.variance.linear(X, thetahat, thetasd, rr, weights = weights)
-  .ci["Lower_CI"]           <- max(0,.ci["Point_Estimate"] - Z*sqrt(.ci["Estimated_Variance"]))
-  .ci["Upper_CI"]           <- min(1,.ci["Point_Estimate"] + Z*sqrt(.ci["Estimated_Variance"]))
+  .ci["Point_Estimate"]     <- pif(X, thetahat, rr, weights = weights, eval.cvx = FALSE)
+  .ci["Estimated_Variance"] <- paf.variance.linear(X, thetahat, thetasd, rr, weights = weights, nsim = nsim)
+  .ci["Lower_CI"]           <- max(.ci["Point_Estimate"] - Z*sqrt(.ci["Estimated_Variance"]), 0)
+  .ci["Upper_CI"]           <- min(.ci["Point_Estimate"] + Z*sqrt(.ci["Estimated_Variance"]), 1)
   
   return(.ci)
   
--- R/paf_variance_linear.R
+++ R/paf_variance_linear.R
@@ -1,6 +1,6 @@
 #' @title Approximate Confidence Intervals for the Potential Impact Fraction
 #' 
-#' @description Function that calculates confidence intervals to the potential impact fraction
+#' @description Function that calculates approximate variance to the potential impact fraction
 #' 
 #' @param X         Random sample (can be vector or matrix) which includes exposure and covariates.
 #' 
@@ -14,6 +14,8 @@
 #' **Optional**
 #' 
 #' @param weights   Survey \code{weights} for the random sample \code{X}
+#' 
+#' @param nsim      Number of simulations
 #' 
 #' @author Rodrigo Zepeda Tello \email{rodrigo.zepeda@insp.mx}
 #' @author Dalia Camacho García Formentí 
@@ -34,6 +36,16 @@
 #' thetavar <- 0.1
 #' paf.variance.linear(X, thetahat, thetavar, function(X, theta){exp(theta*X)})
 #' 
+#' #Example with theta and X multivariate
+#' set.seed(18427)
+#' X1 <- rnorm(1000)
+#' X2 <- rnorm(1000)
+#' X  <- as.matrix(cbind(X1,X2))
+#' thetahat <- c(0.12, 0.03)
+#' thetasd <- matrix(c(0.1, 0, 0, 0.4), byrow = T, nrow = 2)
+#' paf.variance.linear(X, thetahat, thetasd, function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}) 
+#' 
+#' @import MASS
 #' @export
 
 
@@ -63,10 +75,10 @@ paf.variance.linear <- function(X, thetahat, thetasd, rr, weights =  rep(1/nrow(
   #Get expected value and variance of that
   .meanvec   <- rep(NA, .nsim)
   .varvec    <- rep(NA, .nsim)
-  .thetasim  <- rnorm(.nsim, thetahat, thetasd)
+  .thetasim  <- mvrnorm(.nsim, thetahat, thetasd)
   for (i in 1:.nsim){
-    .meanvec[i]  <- .pafexp(.thetasim[i])
-    .varvec[i]   <- .pafvar(.thetasim[i])
+    .meanvec[i]  <- .pafexp(.thetasim[i,])
+    .varvec[i]   <- .pafvar(.thetasim[i,])
   }
   
   #Get variance of that
