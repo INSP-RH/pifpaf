c48c69893cf97dee4d0acc3710df41226574dd31
Dalia Camacho García Formentí <daliaf172@gmail.com>
2016-09-20 11:58:01 -0500

    Add plots and CI
    Added plots of PIF and PAF for different theta values.
    Added confidence intervals for PAF with minimum exposure equal to zero.

---
+++ Shiny Application for PIF/ui.R
@@ -0,0 +1,79 @@
+
+shinyUI(fluidPage( theme = "prueba.css",
+                   withMathJax(),
+                   
+                   titlePanel(h1("PAF and PIF" )),
+                   #Layout
+                   fluidRow(
+                     column(3, h3("Relative Risk Function and Counterfactual Function"),
+                            #Select which are going to be calculated
+                            selectInput(inputId =  "PIForPAF", label = h4("Calculate")
+                                        , choices=list("PAF"= 1, "PIF"=2,
+                                                       "PAF and PIF"=3),
+                                        selected = 1),
+                            uiOutput("PIFselect"),
+                            uiOutput("Cftload"),
+                            uiOutput("aval"),
+                            uiOutput("bval"),
+                            #Select a function
+                            uiOutput("RR"),
+                            #Insert theta values
+                            uiOutput("theta"),
+                            uiOutput("theta0"),
+                            uiOutput("theta1"),
+                            uiOutput("theta2"),
+                            uiOutput("theta3"),
+                            
+                            #Other RR function
+                            
+                            uiOutput("RROther"),
+                            uiOutput("LoadThetahat")),
+                     #Load maximum and minimum values for theta
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     ##########################################################################
+                     column(5, offset = 1,
+                            tabsetPanel(
+                              tabPanel("$$\\text{Numeric results}$$", uiOutput("PAFandPIFout")),
+                              tabPanel("$$\\text{Sensitivity  Plot}$$", 
+                                       h4("Sensitivity Plot"),
+                                       plotOutput("SenstivityPlot")
+                              ),
+                              tabPanel("$$\\text{Plot for different } \\theta \\text{ values (Univariate)}$$",
+                                       textInputRow("minTheta", label = h6("$$ \\text{Minimum } \\theta = $$  "),value = 0),
+                                       textInputRow("maxTheta", label = h6("$$\\text{Maximum } \\theta =$$ "),value = 1),
+                                       plotOutput("PIFPlot")),
+                              tabPanel("$$\\text{Confidence Intervals}$$",
+                                       textInputRow("Conf", label = h6("$$ \\text{Confidence Level } = $$  "),value = 95),
+                                       uiOutput("sdtheta"),
+                                       uiOutput("sdMat"),
+                                       uiOutput("IntervalPAF"))
+                            )),
+                     column(3, offset = 1,
+                            #Load X values
+                            h3("Exposure levels"),
+                            fileInput("X", label = h4("Load file with exposure levels in .csv format"), multiple = FALSE,
+                                      accept =c("text/csv")),
+                            
+                            #Minimum exposure value
+                            radioButtons("xmin0", label = h5("Is the minimum exposure level equal to zero?"),
+                                         choices = list("Yes"=1,"No"=0), selected = 1, inline = TRUE),
+                            uiOutput("Xmin"),
+                            
+                            #Particular weights
+                            radioButtons("w",label = h5("Are there particular weights for each X value?"),
+                                         choices = list("Yes"=1,"No"=0),selected = 0, inline = TRUE),
+                            
+                            uiOutput("weights"))
+                   )
+)
+
+
+
+)
---
+++ Shiny Application for PIF/www/load.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(gridExtra)
+library(pif)
+library(tools)
+library(matrixcalc)
+library(ggplot2)
+library(MASS)
+source('~/Documents/INSP/PAF/ShinyPAF/Shiny Application for PIF/www/textInput.R')
