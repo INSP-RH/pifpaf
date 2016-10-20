1142681ff2896dc4138e98243b051e72e068bc66
Dalia Camacho García Formentí <daliaf172@gmail.com>
2016-10-20 11:08:07 -0500

    Cite where this code comes from

--- R/load.R
+++ R/load.R
@@ -1,6 +1,6 @@
 #' @title Text input row
 #' 
-#' @description Dalia please cite where this comes from
+#' @description Reorganize the display of Input boxes, code comes form answers in http://stackoverflow.com/questions/20637248/shiny-4-small-textinput-boxes-side-by-side
 #' 
 #' @export
 
