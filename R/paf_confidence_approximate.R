27f16ec625afc6fe8adc471b3d3c00dbd8b0293c
Dalia Camacho García Formentí <daliaf172@gmail.com>
2016-10-26 16:08:42 -0500

    Add files via upload

---
+++ R/paf_confidence_approximate.R
@@ -0,0 +1,82 @@
+#' @title Approximate Confidence Intervals for the Population Attributable Fraction
+#' 
+#' @description Function that calculates approximate confidence intervals to the population attributable fraction
+#' 
+#' @param Xmean     Mean value of exposure levels from a previous study.
+#' 
+#' @param Xvar      Variance of exposure levels from a previous study.
+#' 
+#' @param thetahat  Estimative of \code{theta} for the Relative Risk function
+#' 
+#' @param thetavar   Estimator of standard error of thetahat (usually standard error) 
+#' 
+#' @param rr        Function for relative risk
+#' 
+#' 
+#' **Optional**
+#' 
+#' @param nsim      Number of simulations for estimation of variance
+#' 
+#' @param confidence Confidence level \% (default 95)
+#' 
+#' @param check_thetas Checks that theta parameters are correctly inputed
+#' 
+#' @author Rodrigo Zepeda Tello \email{rodrigo.zepeda@insp.mx}
+#' @author Dalia Camacho García Formentí 
+#' 
+#' @examples 
+#' 
+#' #Example 1
+#' set.seed(46987)
+#' rr      <- function(X,theta){exp(X*theta)}
+#' Xmean   <- 3
+#' Xvar    <- 1
+#' theta   <- 0.4
+#' thetasd <- 0.001
+#' .Xmean  <- as.matrix(Xmean)
+#' .Xvar   <- as.matrix(Xvar)
+#' paf.confidence.approximate(Xmean,Xvar,theta,thetasd,rr)
+#' 
+#' #Example 2: Compare paf.variance.approximate with paf.variance.linear
+#' X1       <- rnorm(1000,3,.5)
+#' X2       <- rnorm(1000,4,1)
+#' X        <- as.matrix(cbind(X1,X2))
+#' Xmean    <- colMeans(X)
+#' Xvar     <- cov(X)
+#' .Xmean   <- matrix(Xmean, ncol = length(Xmean))
+#' .Xvar    <- matrix(Xvar, ncol = sqrt(length(Xvar)))
+#' theta    <- c(0.12, 0.17)
+#' thetasd  <- matrix(c(0.001, 0.00001, 0.00001, 0.004), byrow = TRUE, nrow = 2)
+#' rr       <- function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}
+#' paf.confidence.approximate(Xmean,Xvar,theta,thetasd,rr)
+#' paf.confidence.linear(X, theta, thetasd, rr)
+#' 
+#' @export
+
+
+paf.confidence.approximate <- function(Xmean, Xvar, thetahat, thetavar, rr,
+                                  confidence = 95, nsim = 1000, check_thetas = TRUE){
+  
+  #Make thetavar matrix
+  .thetavar <- as.matrix(thetavar)
+  
+  #Function for checking that thetas are correctly inputed
+  if(check_thetas){ check.thetas(.thetavar, thetahat, NA, NA, "approximate") }
+  
+  #Set the vector for the confidence intervals
+  .ci <- c("Lower_CI" = NA, "Point_Estimate" = NA, "Upper_CI" = NA, "Estimated_Variance" = NA)
+  
+  #Set Z for the confidence interval
+  Z <- qnorm(1 - ((100-confidence)/200))
+  
+  #Get the point estimate and variance
+  .ci["Point_Estimate"]     <- pif.approximate(X = Xmean, Xvar = Xvar, thetahat = thetahat, rr = rr)
+  .ci["Estimated_Variance"] <- paf.variance.approximate(Xmean = Xmean, Xvar = Xvar, thetahat = thetahat, thetasd = .thetavar, rr = rr, nsim = nsim)
+  .ci["Lower_CI"]           <- .ci["Point_Estimate"] - Z*sqrt(.ci["Estimated_Variance"])
+  .ci["Upper_CI"]           <- .ci["Point_Estimate"] + Z*sqrt(.ci["Estimated_Variance"])
+  
+  return(.ci)
+  
+}
+
+
---
+++ R/paf_variance_approximate.R
@@ -0,0 +1,112 @@
+#' @title Approximate Variance for the Population Attributable Fraction using the approximate method
+#' 
+#' @description Function that calculates approximate variance to the population attributable fraction.
+#' 
+#' @param Xmean     Mean value of exposure levels from a previous study.
+#' 
+#' @param Xvar      Variance of exposure levels from a previous study.
+#' 
+#' @param thetahat  Estimative of \code{theta} for the Relative Risk function
+#' 
+#' @param thetasd   Estimator of standard deviation of thetahat (usually standard error)
+#' 
+#' @param rr        Function for relative risk
+#' 
+#' 
+#' **Optional**
+#' 
+#' @param nsim      Number of simulations
+#' 
+#' @author Rodrigo Zepeda Tello \email{rodrigo.zepeda@insp.mx}
+#' @author Dalia Camacho García Formentí 
+#' 
+#' @examples 
+#' 
+#' #Example 1
+#' set.seed(46987)
+#' rr      <- function(X,theta){exp(X*theta)}
+#' Xmean   <- 3
+#' Xvar    <- 1
+#' theta   <- 1.2
+#' thetasd <- 0.15
+#' .Xmean  <- as.matrix(Xmean)
+#' .Xvar   <- as.matrix(Xvar)
+#' paf.variance.approximate(Xmean,Xvar,theta,thetasd,rr)
+#' 
+#' #Example 2: Compare paf.variance.approximate with paf.variance.linear
+#' X1       <- rnorm(1000,3,.5)
+#' X2       <- rnorm(1000,4,1)
+#' X        <- as.matrix(cbind(X1,X2))
+#' Xmean    <- colMeans(X)
+#' Xvar     <- cov(X)
+#' .Xmean   <- matrix(Xmean, ncol = length(Xmean))
+#' .Xvar    <- matrix(Xvar, ncol = sqrt(length(Xvar)))
+#' theta    <- c(0.12, 0.17)
+#' thetasd  <- matrix(c(0.001, 0.00001, 0.00001, 0.004), byrow = TRUE, nrow = 2)
+#' rr       <- function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}
+#' paf.variance.approximate(Xmean,Xvar,theta,thetasd,rr)
+#' paf.variance.linear(X, theta, thetasd, rr)
+#' 
+#' 
+#' @import MASS stats numDeriv
+#' @export 
+
+
+paf.variance.approximate <- function(Xmean, Xvar, thetahat, thetasd, rr, nsim = 1000){
+  
+  #Set X as matrix
+  .Xmean  <- matrix(Xmean, ncol = length(Xmean))
+  .Xvar   <- matrix(Xvar, ncol = sqrt(length(Xvar)))
+  #Set a minimum for nsim
+  .nsim        <- max(nsim,10)
+  
+  if(is.positive.definite(.Xvar) == FALSE){
+    stop("Variance matrix must be positive definite.")
+  }
+  
+  #Check exposure values are greater than zero
+  check.exposure(Xmean)
+  
+  #Check that rr is 1 when X = 0
+  check.rr(.Xmean, thetahat, rr)
+  
+  #Rewrite functions as functions of X only
+  rr.fun.x <- function(X){
+    rr(X,thetahat)
+  }
+  
+  #Get the expected paf
+  .pafexp <- function(theta){
+    pif.approximate(Xmean, Xvar, thetahat = theta, rr)
+  }
+  
+  #Get the variance of PAF
+  .pafvar <- function(theta){
+    rr.fun.x <- function(X){
+      rr(X,theta)
+    }
+    
+    dR0 <- as.matrix(grad(rr.fun.x, .Xmean))
+    R0  <- rr(.Xmean, theta)
+    vr  <- t(1/R0^2*dR0)%*%.Xvar%*%(1/R0^2*dR0)
+    return(vr)
+  }
+  
+  #Get expected value and variance of that
+  .meanvec   <- rep(NA, .nsim)
+  .varvec    <- rep(NA, .nsim)
+  .thetasim  <- mvrnorm(.nsim, thetahat, thetasd)
+  for (i in 1:.nsim){
+    .meanvec[i]  <- .pafexp(.thetasim[i,])
+    .varvec[i]   <- .pafvar(.thetasim[i,])
+  }
+  
+  #Get variance of that
+  .varpaf <- var(.meanvec) + mean(.varvec)
+  
+  #Return variance
+  return(.varpaf)
+  
+}
+
+
