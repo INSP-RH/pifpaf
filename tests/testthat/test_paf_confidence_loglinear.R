a52d3e9e25800257db7205f3d76ff3048610cf7b
Dalia Camacho García Formentí <daliaf172@gmail.com>
2016-12-16 12:16:08 -0600

    Change positive definite to positive semi-definite

--- tests/testthat/test_paf_confidence_loglinear.R
+++ tests/testthat/test_paf_confidence_loglinear.R
@@ -12,7 +12,7 @@ test_that("Checking paf.confidence.loglinear",{
     paf.confidence.loglinear(X, thetahat, thetavar, rr, confidence = -1)
   })
   
-  #Expect error  when  variance is not positive definite
+  #Expect error  when  variance is not positive semi-definite
   
   expect_error({
     set.seed(347618)
