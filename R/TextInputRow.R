511436ce97f013409f30a59999410dea838b8dbf
Dalia Camacho García Formentí <daliaf172@gmail.com>
2016-12-16 11:09:47 -0600

    Reference textInputRow

--- R/TextInputRow.R
+++ R/TextInputRow.R
@@ -1,8 +1,8 @@
-#' @title Text input row
+#' @title textInputRow
 #' 
 #' @description This function arranges the display of Input boxes,
-#'  code comes form answers in http://stackoverflow.com/questions/20637248/shiny-4-small-textinput-boxes-side-by-side .
-#' 
+#' Source: Alex Brown, answer on 719016, "shiny 4 small textInput boxes side-by-side", Stack Overflow, January 15 '14, 
+#'         http://stackoverflow.com/a/21132918
 #' @param inputId Input that will be used to acces the value 
 #' @param label   Display label
 #' @param value   Initial value
