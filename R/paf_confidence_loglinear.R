4c660b0624ad9dd15243fbd7d402b6a5cecfb0ed
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2016-12-21 13:22:48 -0600

    Combined approximate and empirical in same file

--- R/paf_confidence_loglinear.R
+++ R/paf_confidence_loglinear.R
@@ -1,7 +1,10 @@
-#' @title Confidence intervals for the Population Attributable Fraction, using the loglinear method
+#' @title Confidence Intervals for the Population Attributable Fraction using loglinear method
 #' 
-#' @description Confidence intervals for the Population Attributable Fraction for relative risk inyective functions, the PAF is inyective, and intervals can be calculated for log(PAF), and then transformed to PAF CI.
-#' @param X         Random sample (can be vector or matrix) which includes exposure and covariates.
+#' @description Confidence intervals for the Population Attributable Fraction.
+#' 
+#' @param X     Mean value of exposure levels from a previous study.
+#' 
+#' @param Xvar      Variance of exposure levels from a previous study.
 #' 
 #' @param thetahat  Estimative of \code{theta} for the Relative Risk function
 #' 
@@ -11,8 +14,6 @@
 #' 
 #' 
 #' **Optional**
-#' 
-#' @param weights   Survey \code{weights} for the random sample \code{X}
 #' 
 #' @param nsim      Number of simulations
 #' 
@@ -24,69 +25,100 @@
 #' @author Dalia Camacho García Formentí 
 #' 
 #' @examples 
-#' 
-#' #Example with risk given by HR
-#' set.seed(18427)
-#' X        <- rnorm(100,5,1)
-#' thetahat <- 0.4
-#' thetavar <- 0.1
-#' paf.confidence.loglinear(X, thetahat, thetavar, function(X, theta){exp(theta*X)})
-#' 
-#' #With larger sample the variance reduces
-#' set.seed(18427)
-#' X        <- rnorm(10000,5,1)
-#' thetahat <- 0.12
-#' thetavar <- 0.1
-#' paf.confidence.loglinear(X, thetahat, thetavar, function(X, theta){exp(theta*X)})
-#' 
-#' #Example with theta and X multivariate
-#' set.seed(18427)
-#' X1       <- rnorm(1000,4,1)
-#' X2       <- rnorm(1000,4,1)
-#' X        <- as.matrix(cbind(X1,X2))
-#' thetahat <- c(0.12, 0.03)
-#' thetavar  <- matrix(c(0.1, 0, 0, 0.4), byrow = TRUE, nrow = 2)
-#' rr       <- function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}
-#' paf.confidence.loglinear(X, thetahat, thetavar, rr) 
-#' 
-#' @import MASS
+#' #Example 1
+#' set.seed(46987)
+#' rr      <- function(X,theta){exp(X*theta)}
+#' X       <- rnorm(100, 3, 0.1)
+#' theta   <- 0.4
+#' thetavar <- 0.001
+#' paf.confidence.loglinear(X, theta, rr, thetavar, Xvar = Xvar, method = "empirical")
+#' paf.confidence.loglinear(mean(X), theta, rr, thetavar, Xvar = var(X), method = "approximate")
+#'
+#'#Example 2: Compare paf.variance.approximate with paf.variance.loglinear
+#'X1       <- rnorm(100,3,.5)
+#'X2       <- rnorm(100,4,1)
+#'X        <- as.matrix(cbind(X1,X2))
+#'Xmean    <- colMeans(X)
+#'Xvar     <- cov(X)
+#'theta    <- c(0.12, 0.17)
+#'thetavar  <- matrix(c(0.001, 0.00001, 0.00001, 0.004), byrow = TRUE, nrow = 2)
+#'rr       <- function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}
+#'paf.confidence.loglinear(X, theta, rr, thetavar)  #Default empirical method
+#'paf.confidence.loglinear(Xmean, theta, rr, thetavar, Xvar = Xvar, method = "approximate")
+#'
+#' @import MASS numDeriv
 #' @export
 
-paf.confidence.loglinear <- function(X, thetahat, thetavar, rr, weights =  rep(1/nrow(as.matrix(X)),nrow(as.matrix(X))),
-                                     nsim = 1000, confidence = 95, check_thetas = TRUE){
+paf.confidence.loglinear <- function(X, thetahat, rr, thetavar,
+                                     weights = rep(1/nrow(as.matrix(X)), nrow(as.matrix(X))),
+                                     confidence = 95, nsim = 1000, check_thetas = TRUE, 
+                                     method = c("empirical", "kernel", "approximate"), Xvar = var(X),
+                                     cft.check = TRUE, ktype = "epanechnikov", bw = "nrd0", adjust = 1,
+                                     npoints = 1000){
   
   #Get confidence
   check.confidence(confidence)
-  .alpha <- max(0, 1 - confidence/100)
-  .Z     <- qnorm(1-.alpha/2)
+  .alpha  <- max(0, 1 - confidence/100)
+  .Z      <- qnorm(1-.alpha/2)
   
   #Get number of simulations
-  .nsim  <- max(10, ceiling(nsim))
+  .nsim   <- max(10, ceiling(nsim))
+  
+  #Get method
+  .method <- method[1]
   
   #Check 
   .thetavar <- as.matrix(thetavar)
   if(check_thetas){ check.thetas(.thetavar, thetahat, NA, NA, "log") }
   
-  #To matrix
-  .X     <- as.matrix(X)
+  #Set X as matrix
+  check.exposure(X)
+  .Xvar   <- check.xvar(Xvar)
+  .X      <- matrix(X, ncol = ncol(.Xvar))
   
   #Calculate the conditional expected value as a function of theta
   .logpafexp <- function(.theta){
-    .RO  <- weighted.mean(rr(.X,.theta), weights)
-    return( -log(.RO) )
+    .paf  <- pif(.X, thetahat = .theta, rr = rr, weights = weights, method = .method, Xvar = .Xvar,
+                 cft.check = cft.check, ktype = ktype, bw = bw, adjust = adjust, npoints = npoints)
+    .R0   <- 1/(1-.paf)
+    return( -log(.R0) )
   }
   
-  #Inverse
-  .inverse <- 1/weighted.mean(rr(.X,thetahat), weights)
-    
+  #Get 1 - paf for point estimate
+  .paf     <- pif(.X, thetahat = thetahat, rr = rr, weights = weights, method = .method, Xvar = .Xvar,
+                  cft.check = cft.check, ktype = ktype, bw = bw, adjust = adjust, npoints = npoints)
+  .inverse <- 1 - .paf
+  
   #Calculate the conditional variance as a function of theta
-  s  <- sum(weights)
-  s2 <- sum(weights^2)
-  .logpafvar <- function(.theta){
-    .RO  <- weighted.mean(rr(.X,.theta), weights)
-    .var <- (1/.RO^2)*( s / (s^2 - s2) ) * weighted.mean((rr(.X,.theta) - .RO)^2, weights)
-    return(.var)
-  }
+  switch(.method,
+         approximate = {
+           .logpafvar <- function(.theta){
+             
+             #Create function of RR  
+             rr.fun.x <- function(X){
+               rr(X, .theta)
+             }
+             
+             #Work with linealization
+             dR0   <- as.matrix(grad(rr.fun.x, .X))
+             R0    <- rr(.X, .theta)
+             .var  <- t(1/R0*dR0)%*%.Xvar%*%(1/R0*dR0)
+             
+             return(.var)
+           }
+         }, empirical = {
+           
+           #Estimate variance
+           .logpafvar <- function(.theta){
+             s  <- sum(weights)
+             s2 <- sum(weights^2)
+             .RO  <- weighted.mean(rr(.X,.theta), weights)
+             .var <- (1/.RO^2)*( s / (s^2 - s2) ) * weighted.mean((rr(.X,.theta) - .RO)^2, weights)
+             return(.var)
+           }
+         }
+  )
+  
   
   #Get expected value and variance of that
   .logmeanvec   <- rep(NA, .nsim)
@@ -101,12 +133,11 @@ paf.confidence.loglinear <- function(X, thetahat, thetavar, rr, weights =  rep(1
   .logvarpaf <- var(.logmeanvec) + mean(.logvarvec)
   
   #Create the confidence intervals
-  .zqrt       <- .Z*sqrt(.logvarpaf)
-  .paf        <- pif(.X, thetahat, rr, weights = weights)
+  .zqrt      <- .Z*sqrt(.logvarpaf)
+
   
   #Compute the PAF intervals
-  .cipaf         <- 1-c("Lower" = .inverse*exp(.zqrt), "Point_Estimate" =  .inverse, 
-                        "Upper" = .inverse*exp(-.zqrt) )
+  .cipaf     <- 1-c("Lower" = .inverse*exp(.zqrt), "Point_Estimate" =  .inverse, "Upper" = .inverse*exp(-.zqrt) )
   
   #Return variance
   return(.cipaf)
