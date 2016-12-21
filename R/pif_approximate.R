bd5fa07feac399f2144b6e2ee909cc719fdee413
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2016-12-21 16:14:23 -0600

    Corrected example

--- R/pif_approximate.R
+++ R/pif_approximate.R
@@ -27,11 +27,9 @@
 #' rr        <- function(X,theta){exp(X*theta)}
 #' cft       <- function(X){0.5*X}
 #' pif.approximate(Xmean, Xvar, thetahat, rr, cft)
-#' pif.approximate(Xmean, Xvar, thetahat, rr)
 #' 
 #' #Example 2: Multivariate
 #' #Multivariate example
-#' 
 #' X1        <- 2
 #' X2        <- 1.1
 #' Xmean     <- as.matrix(cbind(X1,X2))
@@ -39,9 +37,19 @@
 #' Cft       <- function(X){.25*X}
 #' thetahat  <- c(0.12, 0.03)
 #' rr <- function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}
-#' pif.approximate(Xmean, Xvar, thetahat, rr) 
 #' pif.approximate(Xmean, Xvar, thetahat, rr, Cft)
 #' 
+#' #Example 3: More multivariate
+#' X1       <- rnorm(1000,3,.5)
+#' X2       <- rnorm(1000,4,1)
+#' X        <- cbind(X1,X2)
+#' Xmean    <- colMeans(X)
+#' Xvar     <- cov(X)
+#' thetahat <- c(0.12, 0.17)
+#' thetavar <- matrix(c(0.001, 0.00001, 0.00001, 0.004), byrow = TRUE, nrow = 2)
+#' rr       <- function(X, theta){exp(theta[1]*X[,1] + theta[2]*X[,2])}
+#' pif.approximate(Xmean, Xvar, thetahat, rr, cft = function(X){cbind(sqrt(X[,1] + 0.2*X[,2]), X[,1])})
+#'
 #' @import matrixStats numDeriv
 #'  
 #' @export
@@ -51,12 +59,9 @@ pif.approximate <- function(Xmean, Xvar, thetahat, rr,
                             cft = function(Xmean){matrix(0,ncol = ncol(as.matrix(Xmean)), nrow = nrow(as.matrix(Xmean)))},
                             cft.check = TRUE){
   
-  .Xmean  <- matrix(Xmean, ncol = length(Xmean))
-  .Xvar   <- matrix(Xvar, ncol = sqrt(length(Xvar)))
-  
-  if(is.positive.semi.definite(.Xvar) == FALSE){
-    stop("Covariance matrix must be positive semi-definite.")
-  }
+  #Change to matrix
+  .Xvar   <- check.xvar(Xvar)
+  .Xmean  <- matrix(Xmean, ncol = ncol(.Xvar))
   
   #Check exposure values are greater than zero
   check.exposure(.Xmean)
@@ -65,28 +70,27 @@ pif.approximate <- function(Xmean, Xvar, thetahat, rr,
   check.rr(.Xmean, thetahat, rr)
   
   #Check counterfactual
-  if(cft.check){ check.cft(cft, .Xmean) }
+  #if(cft.check){ check.cft(cft, .Xmean) }
   
   #Rewrite functions as functions of X only
-  rr.cft.fun <- function(X){
-    Xcft.value  <- cft(X)
-    rr(Xcft.value,thetahat)
+  rr.cft.fun <- function(xmat){
+    return(rr(cft(xmat),thetahat))
   } 
   
-  rr.fun.x <- function(X){
-    rr(X,thetahat)
+  rr.fun.x <- function(xmat){
+    rr(xmat,thetahat)
   }
   
   
   #Estimate weighted sums
   .mucft   <- rr.cft.fun(.Xmean) + 0.5*EntryMult(hessian(rr.cft.fun,.Xmean), .Xvar)
-  .mux     <- rr.fun.x(.Xmean) + 0.5*EntryMult(hessian(rr.fun.x,.Xmean), .Xvar)
+  .mux     <- rr.fun.x(.Xmean)   + 0.5*EntryMult(hessian(rr.fun.x,.Xmean), .Xvar)
 
   #Check that integrals make sense
   check.integrals(.mux, .mucft)
   
   #Calculate PIF
-  .pif   <- as.numeric(1 - .mucft/.mux)
+  .pif   <- 1 - .mucft/.mux
   
   return(.pif)
   
