a5a3cb8ff29fe71e150177a8bb5eede64712e780
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2016-12-21 15:08:29 -0600

    Added tests for positive_definiteness

--- tests/testthat/test_check_xvar.R
+++ tests/testthat/test_check_xvar.R
@@ -7,10 +7,27 @@ test_that("Checking check.xvar",{
     check.xvar(NA)
   })
   
+  #Check that negative matrices don't work
+  expect_error({
+    check.xvar(matrix(-c(1,0,0,2), byrow = T, ncol = 2))
+  })
+  
+  #Check that non-positive matrices don't work
+  expect_error({
+    check.xvar(matrix(c(-1,0,0,2), byrow = T, ncol = 2))
+  })
+  
+  #Check that matrix is returnes as inputed
   expect_equal(
-    {n <- runif(1)
-    check.xvar(n)},
-    n
+    {
+    mat <- matrix(c(1,0,0,2), byrow = T, ncol = 2)
+    check.xvar(mat)},
+    mat
   )
+  
+  #Check that non-square matrices don't work
+  expect_error({
+    check.xvar(matrix(c(1,2,3,4,5,6), byrow = T, ncol = 3))
+  })
   
 })
