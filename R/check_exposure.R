d652fd8672cbbdc0e8b8a9337cf4344649543bda
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2016-12-21 11:51:46 -0600

    Add files via upload

---
+++ R/check_exposure.R
@@ -0,0 +1,40 @@
+#' @title Check Exposure values
+#' 
+#' @description Function that verifies if all exposure values are greater or equal to zero.
+#' 
+#' @param X     Matrix of exposure that is evaluated in rr
+#' 
+#' 
+#' @examples 
+#' #Example 1 
+#' X <- runif(500, 0,1)
+#' check.exposure(X)
+#' 
+#' 
+#' @export
+
+check.exposure <- function(X){
+  
+  #Boolean variable = 1
+  bool <- TRUE
+  
+  
+  X <- as.matrix(X)
+  m <- dim(X)[1]
+  n <- dim(X)[2]
+  
+  #Check condition
+  i <- 1
+  while(i <= m && bool){
+    j <- 1
+    while(j <= n && bool){
+      if(X[i,j] >= 0){
+        j <- j+1
+      }else{
+        bool <- FALSE
+        warning(paste("Some exposure values are less than zero, verify this is correct."))
+      }
+    }
+    i <- i+1
+  }
+}
