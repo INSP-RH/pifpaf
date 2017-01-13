05c6eced08d9682393de985ecda0962c9ccfb240
Dalia Camacho García Formentí <daliaf172@gmail.com>
2017-01-13 12:00:57 -0600

    Add functions for shiny App

---
+++ R/check_read_Rfun.R
@@ -0,0 +1,25 @@
+#' @title Check an R function has been given with a specific name
+#' 
+#' @description Function that verifies the input is not empty. Verifies the input is an R file. Verifies the R file contained a function with a specific name. If everything is correct returns TRUE, if not a warning label.
+#' 
+#' @param file  Contains file information from "fileInput"
+#' @param fname Name of the function as a string
+#' 
+#' @import tools
+#' @export# 
+check.read.Rfun <- function(file, fname){
+  sol <- TRUE
+  if(is.null(file)){
+    sol <- "warn1"
+  }else if(file_ext(file$name)!="R"){
+    sol <- "warn3"
+  }else{
+    source(file$datapath)
+    if(exists(fname) == FALSE){
+      sol <- "warn4"
+    }else if(is.function(eval(parse(text = fname)))==FALSE){
+      sol <- "warn5"
+    }
+  }
+  return(sol)
+}
---
+++ R/check_read_csv.R
@@ -0,0 +1,19 @@
+#' @title Check csv file was correctly defined in R shiny input
+#' 
+#' @description Function that verifies the input is not empty, and that the input is in a csv format
+#' 
+#' @param file  Contains file information from "fileInput"
+#' 
+#' @import tools
+#' @export# 
+check.read.csv <- function(file){
+  if(is.null(file)){
+    sol <- "warn1"
+  }else if(file_ext(file$name)!="csv"){
+    sol <- "warn2"
+  }else{
+    sol <- as.numeric(as.matrix(read.csv(file$datapath,stringsAsFactors = FALSE)))
+  }
+  return(sol)
+}
+
