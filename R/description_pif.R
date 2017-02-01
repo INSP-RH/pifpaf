40fbe536a2308129f99694f63a42febd2b7b1877
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2017-02-01 17:34:53 -0600

    browseVignettes is now working

---
+++ R/description_pif.R
@@ -0,0 +1,17 @@
+#' pifpaf: A package for estimating the Potential Impact Fraction and the
+#' Population Attributable Fraction from cross-sectional data
+#'
+#' The pifpaf package uses a generalized method to estimate the 
+#' Potential Impact Fraction, (PIF) and the Population Attributable Fraction (PAF) 
+#' from cross-sectional data. It creates point-estimates (\code{\link{pif}}, 
+#' \code{\link{paf}}), confidence intervals (\code{\link{pif.confidence}}, 
+#' \code{\link{paf.confidence}}),and estimates of variance. 
+#' In addition it generates plots for conducting sensitivity analysis. 
+#' This package was developed under funding by Bloomberg 
+#' Philanthropies. Please see the package's vignette for more information:
+#' \code{browseVignettes("pifpaf")}.
+#' 
+#'
+#' @docType package
+#' @name pifpaf
+NULL
